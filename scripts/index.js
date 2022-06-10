57adbf107b5a4ab9f8f948a6c3c8af70b57ad17b
Christian Harjuno <harjuno.tian@gmail.com>
2022-06-11 04:03:13 +0900

    index content and page routing

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const whatsapp = () => {
+	return window.location.replace("https://web.whatsapp.com/");
+};
+const instagram = () => {
+	return window.location.replace("https://www.instagram.com/");
+};
+const facebook = () => {
+	return window.location.replace("https://www.facebook.com/");
+};
+const twitter = () => {
+	return window.location.replace("https://twitter.com/");
+};
+const youtube = () => {
+	return window.location.replace("https://www.youtube.com/");
+};
